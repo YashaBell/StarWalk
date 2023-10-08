40d2df1075d0eb630e3d98028c209844181d745d
YashaBell <zbell@ucsc.edu>
2023-10-08 13:06:52 -0700

    Update Walker.js

--- src/prefabs/Walker.js
+++ src/prefabs/Walker.js
@@ -6,13 +6,10 @@ class Walker extends Phaser.Physics.Arcade.Sprite {
         this.setOrigin(0.5,0.5);
         scene.physics.add.existing(this);
         this.body.onOverlap = true;
-        this.scale = 1;
-        this.body.onOverlap = true;
         this.body.isSquare = true;
         this.body.drag = 0;
         this.alpha = 1;
         this.anims.play('walkLOOP');
-        this.body.onOverlap = true;
         this.scale = 1;
         this.x = 100 + this.width/2;
         this.y = scene.playSpaceY - 100 - this.height/2;
